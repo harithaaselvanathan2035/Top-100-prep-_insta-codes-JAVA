04d10cb62c1569a8f8467507a6b106c1fe4ac01d
Harithaa S <harithaa2035personal@gmail.com>
2025-05-10 14:45:58 +0530

    patterns

---
+++ patters/pattern4.java
@@ -0,0 +1,12 @@
+class pattern4{
+public static void main(String[] args) {
+    int n =4;
+    int m =6;
+    for (int i =0; i<n; i++){
+        for(int j =0; j<m; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+}
---
+++ patters/pattern5.java
@@ -0,0 +1,17 @@
+class pattern5{
+    public static void main(String[] args) {
+        int n =4;
+        int m = 6;
+        for(int i =0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(i == 0 || i == 3 || j == 0 || j == 5){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
